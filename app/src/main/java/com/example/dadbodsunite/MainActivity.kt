74bdec18e5806117d65e9ab97e9972732f775e5e
Morgen Pence <MAPence@student.fullsail.edu>
2023-08-25 15:55:31 -0500

    got the app running now at least. now to fix the links between each page.

--- app/src/main/java/com/example/dadbodsunite/MainActivity.kt
+++ app/src/main/java/com/example/dadbodsunite/MainActivity.kt
@@ -2,34 +2,30 @@ package com.example.dadbodsunite
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dadbodsunite.ui.theme.DadBodsUniteTheme
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_restaurant_layout);
+        setContentView(R.layout.activity_main)
+
         val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
         val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
-        val loginButton = findViewById<Button>(R.id.loginButton)
-
+        loginButton = findViewById<Button>(R.id.loginButton)
 
         loginButton.setOnClickListener {
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
+        }
     }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
